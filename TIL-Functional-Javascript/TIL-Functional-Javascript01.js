d519d1d9320517a7f0a531b2bb12b233730a2a1f
Mark Yoo <yjinhyuk@gmail.com>
2021-10-21 21:00:39 +0900

    Update: example no.2 is now added Detail: apply generator function to iterable operator

--- TIL-Functional-Javascript/TIL-Functional-Javascript01.js
+++ TIL-Functional-Javascript/TIL-Functional-Javascript01.js
@@ -1,3 +1,4 @@
+// 1번 예제
 function* getOddNumInf(num = 0) {
   while (true) {
     num++;
@@ -16,3 +17,31 @@ console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
+
+// 2번 예제
+function* infinity(num = 0) {
+  while (true) yield num++;
+}
+
+function* limitation(num, iter) {
+  for (const i of iter) {
+    yield i;
+    if (i === num) return;
+  }
+}
+
+function* oddNum(num) {
+  for (const i of infinity(1)) {
+    if (i % 2) yield i;
+    if (i === num) return;
+  }
+}
+
+let iter2 = limitation(4, [1, 2, 3, 4, 5, 6]);
+
+console.log(iter2);
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
+console.log(iter2.next());
