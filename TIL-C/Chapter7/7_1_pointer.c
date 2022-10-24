4935ea25846475a49338d91652209a82a65efef1
Mark-Yoo <yjinhyuk@gmail.com>
2022-10-24 22:01:13 +0900

    Update: how to use pointer variables in C added

--- TIL-C/Chapter7/7_1_pointer.c
+++ TIL-C/Chapter7/7_1_pointer.c
@@ -9,5 +9,11 @@ int main(void) {
     printf("포인터 : %p // 값 : %d\n", &b, b);
     printf("포인터 : %p // 값 : %d\n", &c, c);
 
+    int * pointerEx;
+    pointerEx = &b;
+
+    printf("포인터 변수 : %p // 값 : %d", pointerEx, *pointerEx);
+
+    // 포인터 변수는 변수의 주소를 알 수 있다.
     return 0;
 }
