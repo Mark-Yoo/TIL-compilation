74480c69be6ba7c5c2c5119b8379c1ac5d37e7a3
Mark-Yoo <yjinhyuk@gmail.com>
2022-10-27 22:43:47 +0900

    Update : initAnimalName funtion added  for to initialize animal name

--- TIL-C/Chapter8/8_1_multi_dimesion_array.c
+++ TIL-C/Chapter8/8_1_multi_dimesion_array.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
 int arrayAnimal[4][5];
+char * strAnimal[10];
 
 void initAnimalArray(void);
+void initAnimalName(void);
 
 int main(void) {
-
+    initAnimalArray();
+    initAnimalName();
     return 0;
 }
 
 void initAnimalArray(void) {
-    
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            arrayAnimal[i][j] = -1;
+        }
+    }
+}
+
+void initAnimalName(void) {
+    strAnimal[0] = "원숭이";
+    strAnimal[1] = "하마";
+    strAnimal[2] = "강아지";
+    strAnimal[3] = "고양이";
+    strAnimal[4] = "돼지";
+    strAnimal[5] = "코끼리";
+    strAnimal[6] = "기린";
+    strAnimal[7] = "낙타";
+    strAnimal[8] = "타조";
+    strAnimal[9] = "호랑이";
 }
