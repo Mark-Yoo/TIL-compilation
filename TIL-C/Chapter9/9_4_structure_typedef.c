c221bc896cd0cdcf5bbccdf7fd8c2240648ce724
Mark-Yoo <yjinhyuk@gmail.com>
2022-11-05 22:03:16 +0900

    feat : another way to use struct method

--- TIL-C/Chapter9/9_4_structure_typedef.c
+++ TIL-C/Chapter9/9_4_structure_typedef.c
@@ -8,10 +8,22 @@ struct Info {
     struct Info * subInfo;
 };
 
+typedef struct {
+    char * name;
+    int year;
+    int price;
+    char * company;
+    struct Info * subInfo;
+} INFO_STRUCT;
+
 int main(void) {
     typedef struct Info helpfulInfo;
     helpfulInfo info1;
     info1.name = "Mark";
     info1.year = 2022;
+
+    INFO_STRUCT info2;
+    info2.name = "Julie";
+    info2.year = 2022;
     return 0;
 }
