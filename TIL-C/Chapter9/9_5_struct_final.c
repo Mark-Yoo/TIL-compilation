6ca937877f979e819d3f4000b2b848318320cd3a
Mark-Yoo <yjinhyuk@gmail.com>
2022-11-07 21:12:56 +0900

    Update: struct final project added init data

--- TIL-C/Chapter9/9_5_struct_final.c
+++ TIL-C/Chapter9/9_5_struct_final.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 typedef struct {
     char * name;
@@ -8,6 +10,8 @@ typedef struct {
 } CAT;
 
 CAT cats[5];
+int collections[5] = { 0, 0, 0, 0, 0 };
+
 void initCats();
 
 int main(void) {
@@ -20,4 +24,20 @@ void initCats(void) {
     cats[0].age = 5;
     cats[0].character = "상냥함";
     cats[0].level = 1;
+    cats[1].name = "콩콩이";
+    cats[1].age = 3;
+    cats[1].character = "온순함";
+    cats[1].level = 2;
+    cats[2].name = "점박이";
+    cats[2].age = 3;
+    cats[2].character = "게으름";
+    cats[2].level = 3;
+    cats[3].name = "호랭이";
+    cats[3].age = 5;
+    cats[3].character = "예민함";
+    cats[3].level = 4;
+    cats[4].name = "하울이";
+    cats[4].age = 2;
+    cats[4].character = "호기로움";
+    cats[4].level = 5;
 }
