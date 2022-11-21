d83fb9683885e7189acd57758a54808e8d1c41d4
Mark-Yoo <yjinhyuk@gmail.com>
2022-11-21 23:02:00 +0900

    feat: macro added

--- TIL-C/Chapter10/10_2_read_data_from_file.c
+++ TIL-C/Chapter10/10_2_read_data_from_file.c
@@ -1,11 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define MAX 10000
 
 int main(void) {
+    char line[MAX];
     FILE * file = fopen("c:\\Users\\사용자계절\\test1.txt", "rb");
     if (file = NULL) {
-        printf("파일 열기 실패");
+        printf("파일 열기 실패\n");
         return 1;
     }
+
+    while ((fgets, MAX, file) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(file);
     return 0;
 }
