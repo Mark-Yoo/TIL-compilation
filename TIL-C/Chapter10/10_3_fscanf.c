c14fb373013f44e301c97a2e16db56c97bd99e5f
Mark-Yoo <yjinhyuk@gmail.com>
2022-11-25 23:06:38 +0900

    feat: in case of fopen file fail added

--- TIL-C/Chapter10/10_3_fscanf.c
+++ TIL-C/Chapter10/10_3_fscanf.c
@@ -5,4 +5,11 @@
 int main(void) {
     char str1[MAX];
     char str[MAX];
+    int num[6] = { 0, 0, 0, 0, 0, 0 };
+    int bonus = 0;
+    FILE * file = fopen("사용자 디렉토리", "rb");
+    if (file == NULL) {
+        printf("파일 열기 실패\n");
+        return 1;
+    }
 }
