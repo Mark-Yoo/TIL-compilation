1802902be0729dbe0daf7665e4a5c3c38a49a209
Mark-Yoo <yjinhyuk@gmail.com>
2022-11-27 22:46:30 +0900

    feat : print all results from file

--- TIL-C/Chapter10/10_3_fscanf.c
+++ TIL-C/Chapter10/10_3_fscanf.c
@@ -4,7 +4,7 @@
 
 int main(void) {
     char str1[MAX];
-    char str[MAX];
+    char str2[MAX];
     int num[6] = { 0, 0, 0, 0, 0, 0 };
     int bonus = 0;
     FILE * file = fopen("사용자 디렉토리", "rb");
@@ -12,4 +12,11 @@ int main(void) {
         printf("파일 열기 실패\n");
         return 1;
     }
+
+    fscanf(file, "%s %d %d %d %d %d %d", str1, &num[0], &num[1], &num[2], &num[3], &num[4], &num[5]);
+    printf("%s %d %d %d %d %d %d\n", str1, &num[0], &num[1], &num[2], &num[3], &num[4], &num[5]);
+    fscanf(file, "%s %d", str2, &bonus);
+    printf("%s %d\n", str2, bonus);
+    fclose(file);
+    return 0;
 }
