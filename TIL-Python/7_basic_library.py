9c7e967a45fb537743b2423fb01a7b8d52e9a6aa
Mark-Yoo <yjinhyuk@gmail.com>
2022-12-12 09:42:34 +0900

    feat : add 2 arguments for graph

--- TIL-Python/7_basic_library.py
+++ TIL-Python/7_basic_library.py
@@ -1,3 +1,9 @@
 import matplotlib.pyplot as plt
 plt.plot([1, 5, 7, 3, 7])
 plt.show()
+
+sales = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+month = [ 3, 5, 4, 7, 8, 5, 6]
+plt.title('Monthly sales')
+plt.plot(sales, month)
+plt.show()
