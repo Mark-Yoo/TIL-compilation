100543890b9c29889d33a942725fd8ff42ba7fec
Mark-Yoo <yjinhyuk@gmail.com>
2022-12-13 09:19:00 +0900

    feat : change size, attach label on graph

--- TIL-Python/7_basic_library.py
+++ TIL-Python/7_basic_library.py
@@ -4,6 +4,8 @@ plt.show()
 
 sales = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
 month = [ 3, 5, 4, 7, 8, 5, 6]
+plt.figure(dpi=150)
 plt.title('Monthly sales')
-plt.plot(sales, month)
+plt.plot(sales, month, color='y', label='stock level')
+plt.legend(loc='lower right')
 plt.show()
