3f1a30e5bb8d1ba9c8d4c443d9a6f10ad764a766
Mark-Yoo <yjinhyuk@gmail.com>
2022-12-13 09:34:22 +0900

    feat : bar type graph added

--- TIL-Python/7_basic_library.py
+++ TIL-Python/7_basic_library.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 plt.plot([1, 5, 7, 3, 7])
 plt.show()
 
+# 꺾은선 그래프
 sales = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
 month = [ 3, 5, 4, 7, 8, 5, 6]
 plt.figure(dpi=150)
@@ -10,4 +11,12 @@ plt.plot(sales, month, color='y', label='stock level')
 plt.legend(loc='lower right')
 plt.xlabel('month')
 plt.ylabel('amount')
+plt.show()
+
+# bar형 그래프
+sales = [1, 3, 6, 5, 7]
+# month = [3, 4, 5, 6, 7]
+plt.title('Monthly Sales')
+# plt.bar(range(3, 8), sales, color='b')
+plt.barh(range(3, 8), sales, color='b')
 plt.show()
