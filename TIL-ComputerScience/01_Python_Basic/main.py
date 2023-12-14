daf1ed1ab452d318ab7c39ca60a14c16f4cb7605
Mark-Yoo <yjinhyuk@gmail.com>
2023-12-14 20:51:23 +0900

    feat: python basic list control function

---
+++ TIL-ComputerScience/01_Python_Basic/main.py
@@ -0,0 +1,31 @@
+a = [1, 3, 2, 5, 4]
+b = [1, 2, 3, 4]
+
+# Append new value to list (last index)
+# a.append(7)
+
+# Reverse List
+# a.reverse()
+
+# Sort List ASC
+# a.sort()
+
+# Find value on specific index(arg)
+# a.index(2)
+
+# Delete value based on index
+# del a[2]
+
+# Insert value(arg2) on specific index(arg1)
+# a.insert(4, 6)
+
+# Remove specific value(arg) from the list
+# a.remove(4)
+
+# Pop value out from the list (last index)
+# a.pop()
+
+# Combine two lists into one
+# a.extend(b)
+
+print(a)
