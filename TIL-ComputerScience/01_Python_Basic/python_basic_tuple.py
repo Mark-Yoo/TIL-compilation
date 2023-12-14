5b4bb95286fb7866bd2f0c8b34daf0e3d1d0113c
Mark-Yoo <yjinhyuk@gmail.com>
2023-12-14 21:34:42 +0900

    feat: python basic knowledge about tuple

---
+++ TIL-ComputerScience/01_Python_Basic/python_basic_tuple.py
@@ -0,0 +1,16 @@
+# tuple basic format
+# if there is only one property in tuple, requires comma afterward
+# brackets also can be removed
+# it's property cannot be changed
+
+a = (1,)
+b = (1, 2)
+c = (1, 2, 3)
+d = 1,2,3
+e = tuple()
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
