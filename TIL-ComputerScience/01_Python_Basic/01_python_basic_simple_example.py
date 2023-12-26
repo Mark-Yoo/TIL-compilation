d90754edf052e5a9d59713cfff3a52b3579e1da1
Mark-Yoo <yjinhyuk@gmail.com>
2023-12-26 21:46:29 +0900

    feat : repeatation examples added

--- TIL-ComputerScience/01_Python_Basic/01_python_basic_simple_example.py
+++ TIL-ComputerScience/01_Python_Basic/01_python_basic_simple_example.py
@@ -2,3 +2,28 @@ for i in range(1, 10):
     for j in range(1, 10):
         print('{} x {} = {}'.format(i, j, i * j), end='\t')
     print('\n')
+
+
+# Tuple
+for m in (1, 2, 3):
+    print('m : {}'.format(m), end='\t')
+print('\n')
+
+# String
+lines = 'hello'
+for s in lines:
+    print('s : {}'.format(s), end='\t')
+print('\n')
+
+# List
+li = [1, 2, 3]
+for n in li:
+    print('n : {}'.format(n), end='\t')
+print('\n')
+
+# Get multiples of 4
+for l in range(1, 7):
+    for o in range(1, 7):
+        k = l + o
+        if k % 4 == 0:
+            print('{} + {} = {}'.format(l, o, l + o))
