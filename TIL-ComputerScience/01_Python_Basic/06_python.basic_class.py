bf5aa4380f7eb9a9a6afaf1f5119f31d305ba752
Mark-Yoo <yjinhyuk@gmail.com>
2024-01-03 19:59:47 +0900

    feat: build basic class with python

---
+++ TIL-ComputerScience/01_Python_Basic/06_python.basic_class.py
@@ -0,0 +1,16 @@
+class TestClass:
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def get_age(self):
+        print("I am", self.age)
+
+    def get_name(self):
+        print("My name is", self.name)
+
+
+test = TestClass("Peter", 185, 25)
+test.get_age()
+test.get_name()
