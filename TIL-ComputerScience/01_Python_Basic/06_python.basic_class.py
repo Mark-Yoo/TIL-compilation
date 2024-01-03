f27562f20a52db44beb82435138a97a0d3395993
Mark-Yoo <yjinhyuk@gmail.com>
2024-01-03 20:27:35 +0900

    add: basic use and descrition about @classmethod, @staticmethod added

--- TIL-ComputerScience/01_Python_Basic/06_python.basic_class.py
+++ TIL-ComputerScience/01_Python_Basic/06_python.basic_class.py
@@ -1,4 +1,6 @@
 class TestClass:
+    count = 0
+
     def __init__(self, name, height, age):
         self.name = name
         self.height = height
@@ -10,6 +12,16 @@ class TestClass:
     def get_name(self):
         print("My name is", self.name)
 
+    @staticmethod
+    def test_static():
+        print("Testing Static Methods")
+
+    # Class method can get to other variables inside class when staticmethod can't
+    @classmethod
+    def test_class_method(cls):
+        print("class method")
+        print("count :::", cls.count)
+
     def __del__(self):
         print("Into the dust")
 
@@ -17,3 +29,5 @@ class TestClass:
 test = TestClass("Peter", 185, 25)
 test.get_age()
 test.get_name()
+TestClass.test_static()
+TestClass.test_class_method()
