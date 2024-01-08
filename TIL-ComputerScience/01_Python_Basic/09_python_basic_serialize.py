0fbb710647221807e48c2edf8347a1a36029e81d
Mark-Yoo <yjinhyuk@gmail.com>
2024-01-08 19:11:35 +0900

    feat: python serialization - pickle

---
+++ TIL-ComputerScience/01_Python_Basic/09_python_basic_serialize.py
@@ -0,0 +1,11 @@
+# Serialization : Transfrom object into continuous data
+# serialization of object is needed for to save on computer
+
+# pickle module is used on python to serialize objects
+
+import pickle
+with open('test.txt','wb') as f:
+    print(pickle.dump([1, 2, 3, 4, 5], f))
+
+print(pickle.dumps([1, 2, 3, 4]))
+print(pickle.loads(b'\x80\x04\x95\r\x00\x00\x00\x00\x00\x00\x00]\x94(K\x01K\x02K\x03K\x04e.'))
